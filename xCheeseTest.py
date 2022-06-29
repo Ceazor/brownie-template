fe02a6f04f2e35c7fe98ca1c1db4338a518e5325
Ceazor <ceahorse@gmail.com>
2022-06-29 23:37:23 +0800

    adding xCheese

---
+++ xCheeseTest.py
@@ -0,0 +1,12 @@
+
+brownie console --network ftm-main-fork
+
+
+vault = Contract("0x58E0ac1973F9d182058E6b63e7F4979bc333f493")
+xCheese = Contract("0xAe71E0AeADa3bf9a188f06464528313Ce8D3E740")
+cre8r = Contract("0x2aD402655243203fcfa7dCB62F8A08cc2BA88ae0")
+owner = ("0x699675204aFD7Ac2BB146d60e4E3Ddc243843519")
+hotwallet = ("0xA67D2c03c3cfe6177a60cAed0a4cfDA7C7a563e0")
+
+
+
