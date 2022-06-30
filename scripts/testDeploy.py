fcef89f9c336e8a424552799b1d5a8d6c56b6c5e
Ceazor <ceahorse@gmail.com>
2022-06-30 21:53:56 +0800

    env changes

--- scripts/testDeploy.py
+++ scripts/testDeploy.py
@@ -1,3 +1,5 @@
+brownie console --network ftm-main-fork
+
 ceazfBEETs = Contract("0x58E0ac1973F9d182058E6b63e7F4979bc333f493")
 xCheese = Contract("0xAe71E0AeADa3bf9a188f06464528313Ce8D3E740")
 cre8r = Contract("0x2aD402655243203fcfa7dCB62F8A08cc2BA88ae0")
